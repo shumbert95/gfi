952814a7894c312d6288882519d072f56d2c560e
Simon Humbert <shumbert@kernix.com>
2017-04-26 13:54:19 +0200

    Get quizz by offer reference

--- src/AppBundle/Controller/API/QuizController.php
+++ src/AppBundle/Controller/API/QuizController.php
@@ -55,4 +55,44 @@ class QuizController extends Controller
 
         return new JsonResponse($response);
     }
+
+    /**
+     * @Get("/quiz/{offer_reference}")
+     */
+    public function getQuizByOfferAction(Request $request)
+    {
+        $offer = $this->get('doctrine.orm.entity_manager')
+            ->getRepository('AppBundle:Offer')
+            ->findOneByReference($request->get('offer_reference'));
+        if ($offer) {
+            $quizs = $offer->getQuizs();
+            $response['success'] = 'true';
+            foreach ($quizs as $quiz) {
+                foreach ($quiz->getQuestions() as $question) {
+                    $questions[] = array('id' => $question->getId(),
+                        'title' => $question->getTitle(),
+                        'answer_one' => $question->getAnswerOne(),
+                        'answer_two' => $question->getAnswerTwo(),
+                        'answer_three' => $question->getAnswerThree(),
+                        'answer_four' => $question->getAnswerFour(),
+                        'good_answer' => $question->getGoodAnswer(),
+                        'points' => $question->getPoints(),
+                        'time' => $question->getTime(),
+                    );
+                }
+                $response['quiz'][] = array(
+                    'id' => $quiz->getId(),
+                    'name' => $quiz->getTitle(),
+                    'poste' => $quiz->getPoste(),
+                    'questions' => isset($questions) ? $questions : '',
+                );
+            }
+
+        } else {
+            $response = array('success' => 'false', 'message' => 'No offer found.');
+        }
+
+        return new JsonResponse($response);
+
+    }
 }
