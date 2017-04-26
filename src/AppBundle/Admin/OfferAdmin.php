2b2006c29fed9f59aa52dfab8e12e0c81a2d910b
Simon Humbert <shumbert@kernix.com>
2017-04-26 14:45:50 +0200

    Add Offer Admin tab

---
+++ src/AppBundle/Admin/OfferAdmin.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace AppBundle\Admin;
+
+use AppBundle\Entity\Quiz;
+use AppBundle\Entity\Tags;
+use Sonata\AdminBundle\Admin\AbstractAdmin;
+use Sonata\AdminBundle\Datagrid\ListMapper;
+use Sonata\AdminBundle\Datagrid\DatagridMapper;
+use Sonata\AdminBundle\Form\FormMapper;
+
+class OfferAdmin extends AbstractAdmin
+{
+    protected function configureFormFields(FormMapper $formMapper)
+    {
+        $formMapper->add('reference', 'text', ['label' => 'Reference', 'required' => true]);
+        $formMapper->add('title', 'text', ['label' => 'Titre', 'required' => true]);
+        $formMapper->add('poste', 'text', ['label' => 'Poste', 'required' => true]);
+        $formMapper->add('description', 'text', ['label' => 'Description', 'required' => true]);
+        $formMapper->add('required_skills', 'entity', ['required' => true,
+            'label' => 'Compétences requises',
+            'multiple' => true,
+            'class' => Tags::class]);
+        $formMapper->add('optional_skills', 'entity', ['required' => false,
+            'label' => 'Compétences optionnelles',
+            'multiple' => true,
+            'class' => Tags::class]);
+        $formMapper->add('quizs', 'entity', ['required' => false,
+            'label' => 'Questionnaires',
+            'multiple' => true,
+            'class' => Quiz::class]);
+    }
+
+    protected function configureDatagridFilters(DatagridMapper $datagridMapper)
+    {
+        $datagridMapper->add('id');
+        $datagridMapper->add('reference');
+        $datagridMapper->add('poste');
+        $datagridMapper->add('title');
+    }
+
+    public function configureListFields(ListMapper $list)
+    {
+        $list->addIdentifier('id', null, ['label' => 'Id']);
+        $list->addIdentifier('reference', null, ['label' => 'Reference']);
+        $list->addIdentifier('poste', null, ['label' => 'Poste']);
+        $list->addIdentifier('title', null, ['label' => 'Titre']);
+    }
+}
