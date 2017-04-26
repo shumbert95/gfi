4e073f2129838519cfdfa46a6771447ff63da55d
Simon Humbert <shumbert@kernix.com>
2017-04-26 15:28:41 +0200

    Add Tag Tab in BO

---
+++ src/AppBundle/Admin/TagAdmin.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace AppBundle\Admin;
+
+use AppBundle\Entity\Question;
+use Sonata\AdminBundle\Admin\AbstractAdmin;
+use Sonata\AdminBundle\Datagrid\ListMapper;
+use Sonata\AdminBundle\Datagrid\DatagridMapper;
+use Sonata\AdminBundle\Form\FormMapper;
+
+class TagAdmin extends AbstractAdmin
+{
+    protected function configureFormFields(FormMapper $formMapper)
+    {
+        $formMapper->add('name', 'text', ['label' => 'Name', 'required' => true]);
+    }
+
+    protected function configureDatagridFilters(DatagridMapper $datagridMapper)
+    {
+        $datagridMapper->add('id');
+        $datagridMapper->add('name');
+    }
+
+    public function configureListFields(ListMapper $list)
+    {
+        $list->addIdentifier('id', null, ['label' => 'Id']);
+        $list->addIdentifier('name', null, ['label' => 'Nom']);
+    }
+}
