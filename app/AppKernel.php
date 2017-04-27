57868137fc9e3d086d1905ab0b579e9d7939e460
Simon Humbert <shumbert@kernix.com>
2017-04-27 14:18:11 +0200

    Add CKEditor bundle

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -8,6 +8,7 @@ class AppKernel extends Kernel
     public function registerBundles()
     {
         $bundles = [
+            new Ivory\CKEditorBundle\IvoryCKEditorBundle(),
             new Symfony\Bundle\FrameworkBundle\FrameworkBundle(),
             new Symfony\Bundle\SecurityBundle\SecurityBundle(),
             new Symfony\Bundle\TwigBundle\TwigBundle(),
